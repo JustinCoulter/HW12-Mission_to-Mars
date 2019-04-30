55a0237c479212fc8e49c3e42628140371dd4a87
Justin Coulter <justincoulter@icloud.com>
2019-04-30 10:44:26 -0400

    added template files for project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+
+
+
+@app.route("/scrape")
+def scraper():
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
